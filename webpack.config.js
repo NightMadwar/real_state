8eb33b291d7528ffbd2c3a6ea64949d0d6b1a2bc
Madwar215 <Mahermadwar2001@hotmail.com>
2024-08-07 17:39:01 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/main.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+};
