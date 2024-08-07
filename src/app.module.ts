5b7b8c35eeb2916d3d165c6ce5eb29da3a2cc406
Madwar215 <Mahermadwar2001@hotmail.com>
2024-08-07 17:27:05 +0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { PropertyMediaModule } from './property-media/property-media.module';
 import { AppointmentModule } from './appointment/appointment.module';
 import { PaymentModule } from './payment/payment.module';
 import { AuthModule } from './auth/auth.module';
-import { BookModule } from './book/book.module';
+
 import { NestFactory } from '@nestjs/core';
 import { LoggingMiddleware } from './common/middleware/logging/logging.middleware';
 import { AdminModule } from './admin/admin.module';
@@ -36,7 +36,6 @@ import { AdminModule } from './admin/admin.module';
     AppointmentModule,
     PaymentModule,
     AuthModule,
-    BookModule,
     AdminModule,
   ],
 })
